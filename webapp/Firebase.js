2820520d049bb79b656abf94804a3d905836bada
Freddy Jetty Johnson <freddy.jetty.johnson@sap.com>
2020-07-04 15:39:26 +0530

    Removed credentials

--- webapp/Firebase.js
+++ webapp/Firebase.js
@@ -1,22 +1,13 @@
 sap.ui.define([
     "sap/ui/model/json/JSONModel",
-], function (JSONModel) {
+    "elite/FirebaseConfig"
+], function (JSONModel, FirebaseConfig) {
     "use strict";
-
-    var firebaseConfig = {
-        apiKey: "AIzaSyAPvPi1Vqb-W-YunrXSkduRBDyYn-xATWk",
-        authDomain: "elite-box.firebaseapp.com",
-        databaseURL: "https://elite-box.firebaseio.com",
-        projectId: "elite-box",
-        storageBucket: "elite-box.appspot.com",
-        messagingSenderId: "454266096372",
-        appId: "1:454266096372:web:0ec81db9e53c428df12ca1"
-      };
 
     return {
         initializeFirebase: function () {
             // Initialize Firebase with the Firebase-config
-            firebase.initializeApp(firebaseConfig);
+            firebase.initializeApp(FirebaseConfig);
             // firebase.analytics();
 
             // Create a Firestore reference
